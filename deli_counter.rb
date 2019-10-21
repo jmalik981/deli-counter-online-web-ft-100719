25ec7acfa6d6be650810ebcf2e68f007f70d328b
Junaid Malik <jmalik981@gmail.com>
2019-10-21 18:18:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,12 @@
-# Write your code here.
 def line(katz_deli)
+  other_deli=[]
+  if katz_deli.length==0 
   puts "The line is currently empty."
+  else 
+    other_deli<< katz_deli.each.with_index do |person,i|
+      katz_deli.join(" ")
+      puts "The line is currently: " + "#{i+1}. #{person}"
+    end
+    other_deli
+  end
 end
-
-def line(other_deli)
-  puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
